9585a9d680c59e1611c249bc14db0cda73fae1dd
manu84-bit <megaskatos@gmail.com>
2024-05-11 23:05:14 -0500

    Moving and dragging bugs solved

--- js/puzzle.js
+++ js/puzzle.js
@@ -1,38 +1,41 @@
-var box = document.getElementById("square");
+
+window.onload = ()=>{
+
 var squarePiece = document.getElementById("piece-wrapper1");
 var wrappers = document.getElementsByClassName("piece-wrapper");
+var canvas = document.getElementById("canvas");
+var initX, initY, mousePressX, mousePressY;
 
-
-var initX, initY, mousePressX, mousePressY, initW, initH, initRotate;
+canvas.width = window.innerWidth - 20;
+canvas.height = window.innerHeight - 20;
 
 function repositionElement(x, y, wrapper) {
     wrapper.style.left = x + 'px';
     wrapper.style.top = y + 'px';
 }
 
-function getCurrentRotation(el) {
-    var st = window.getComputedStyle(el, null);
-    var tm = st.getPropertyValue("-webkit-transform") ||
-        st.getPropertyValue("-moz-transform") ||
-        st.getPropertyValue("-ms-transform") ||
-        st.getPropertyValue("-o-transform") ||
-        st.getPropertyValue("transform")
-    "none";
-    if (tm != "none") {
-        var values = tm.split('(')[1].split(')')[0].split(',');
-        var angle = Math.round(Math.atan2(values[1], values[0]) * (180 / Math.PI));
-        return (angle < 0 ? angle + 360 : angle);
-    }
-    return 0;
-}
+// function getCurrentRotation(el) {
+//     var st = window.getComputedStyle(el, null);
+//     var tm = st.getPropertyValue("-webkit-transform") ||
+//         st.getPropertyValue("-moz-transform") ||
+//         st.getPropertyValue("-ms-transform") ||
+//         st.getPropertyValue("-o-transform") ||
+//         st.getPropertyValue("transform")
+//     "none";
+//     if (tm != "none") {
+//         var values = tm.split('(')[1].split(')')[0].split(',');
+//         var angle = Math.round(Math.atan2(values[1], values[0]) * (180 / Math.PI));
+//         return (angle < 0 ? angle + 360 : angle);
+//     }
+//     return 0;
+// }
 
-function rotateBox(deg, el) {
-    el.style.transform = `rotate(${deg}deg)`;
-}
 
 // drag support
 function draggable(el){
     el.addEventListener('mousedown', function (event) {
+        event.preventDefault();
+
         if (event.target.className.indexOf("dot") > -1) {
             return;
         }
@@ -52,21 +55,73 @@ function draggable(el){
         el.addEventListener('mouseup', function eventEndHandler() {
             el.removeEventListener('mousemove', eventMoveHandler, false);
             window.removeEventListener('mouseup', eventEndHandler);
+            el.removeEventListener('mouseup', eventEndHandler);
+
         }, false);
     
     }, false);
 }
 
-for(let i=0; i < wrappers.length; i++){
-    draggable(wrappers[i]);
+
+// for(let i=0; i < wrappers.length; i++){
+//     draggable(wrappers[i]);
+// }
+
+function dragElement(elmnt) {
+    var pos1 = 0,
+      pos2 = 0,
+      pos3 = 0,
+      pos4 = 0;
+    elmnt.onmousedown = dragMouseDown;
+  
+    function dragMouseDown(e) {
+      e.preventDefault();
+      //To prevent movement when rotating
+      if (e.target.className.indexOf("dot") > -1) {
+        return;
+    }
+
+      // get the mouse cursor position at startup:
+     
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+      elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+
+
+    }
+    function closeDragElement() {
+      /* stop moving when mouse button is released:*/
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
+
+  for(let i=0; i < wrappers.length; i++){
+    dragElement(wrappers[i]);
 }
-
-
-
 // handle rotation
+function rotateBox(deg, el) {
+    el.style.transform = `rotate(${deg}deg)`;
+}
 
 function rotable(el, querySelector, rotateId){
     var rotate = document.getElementById(rotateId);
+
+    
 rotate.addEventListener('mousedown', function (event) {
   
     initX = this.offsetLeft;
@@ -104,13 +159,13 @@ for(let i=1; i < wrappers.length; i++){
 
 //Reposition pieces 
 var posX = 100
-var posY = 100
+var posY = 120
 for(i= 0 ; i < wrappers.length; i++){
     if( i>0){
-        posY = 250;
+        posY = 240;
     }
     if(i>=3){
-        posY = 400;
+        posY = 340;
     }
     if(i==2 || i == 4){
         posX = 250;
@@ -119,4 +174,5 @@ for(i= 0 ; i < wrappers.length; i++){
         posX = 100;
     }
     repositionElement(posX, posY, wrappers[i]);
+}
 }
